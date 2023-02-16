9bd93081d7b08a48d3b41dcebd4a67acd76226ef
elifcosgun <elifcosgunc@gmail.com>
2023-02-16 18:35:49 +0300

    animation system added
    move and run animation blend trees added
    
    paladin sword and shield animations added

---
+++ Assets/Code/Scripts/Animation_Scripts/AnimationSystemBase.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class AnimationSystemBase : MonoBehaviour
+{
+    [HideInInspector] private Animator animator;
+    //[SerializeField] private float attackSpeed;
+
+    //private CharacterBase characterBase;
+
+    protected virtual void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    protected virtual void Start()
+    {
+        //SetAttackSpeed(1);
+    }
+
+    //public AnimationSystemBase Init(CharacterBase characterBase)
+    //{
+    //    this.characterBase = characterBase;
+    //    return this;
+    //}
+
+    public virtual void AttackAnimation(string key, bool isTrue = false, AnimationClip animationClip = null)
+    {
+        if (!animator)
+            return;
+
+        animator.SetTrigger(key);
+    }
+
+    public virtual void SetMove(string keyX, float valueX, string keyY, float valueY)
+    {
+        if (!animator)
+            return;
+
+        animator.SetFloat(keyX, valueX);
+        animator.SetFloat(keyY, valueY);
+    }
+
+    public virtual void SetMove(string keyX, float valueX, string keyY, float valueY, string keyZ, float valueZ)
+    {
+        if (!animator)
+            return;
+
+        animator.SetFloat(keyX, valueX);
+        animator.SetFloat(keyY, valueY);
+        animator.SetFloat(keyZ, valueZ);
+    }
+
+    public virtual void SetMove(string keyX, float valueX, string keyY, float valueY, bool isRun)
+    {
+        if (!animator)
+            return;
+
+        animator.SetFloat(keyX, valueX);
+        animator.SetFloat(keyY, valueY);
+
+        if (isRun)
+        {
+            animator.SetFloat(AnimationKey.WalkBlend, +0.1f);
+        }
+    }
+
+    //public virtual void SkillAnimation(string key, AnimationClip animationClip = null)
+    //{
+    //    if (!animator)
+    //        return;
+
+    //    animator.SetTrigger(key);
+    //}
+
+    public virtual void SetBool(string key, bool value)
+    {
+        if (!animator)
+            return;
+
+        animator.SetBool(key, value);
+    }
+
+    public virtual void SetFloat(string key, float value)
+    {
+        if (!animator)
+            return;
+
+        animator.SetFloat(key, value);
+    }
+
+    public virtual void SetTrigger(string key)
+    {
+        if (!animator)
+            return;
+
+        animator.SetTrigger(key);
+    }
+
+    //private Coroutine temporaryAttackSpeed, temporaryMovementSpeed;
+    //private GameObject effectObject;
+
+    //public void SetAttackSpeed(float effect, bool isTemporary = false, float duration = 0, GameObject slowObject = null)
+    //{
+    //    animator.SetFloat(AnimationKey.AttackSpeed, attackSpeed * effect);
+
+    //    if (isTemporary && gameObject.activeInHierarchy)
+    //    {
+    //        if (temporaryAttackSpeed != null)
+    //            StopCoroutine(temporaryAttackSpeed);
+
+    //        temporaryAttackSpeed = StartCoroutine(Duration());
+
+    //        //if (!effectObject)
+    //        //{
+    //        //    effectObject = Instantiate(slowObject, transform);
+    //        //    effectObject.transform.position = characterBase.posParticle.position;
+    //        //}
+
+    //        IEnumerator Duration()
+    //        {
+    //            yield return new WaitForSeconds(duration);
+    //            animator.SetFloat(AnimationKey.AttackSpeed, attackSpeed);
+    //            if (effectObject)
+    //                Destroy(effectObject);
+    //        }
+    //    }
+    //}
+
+    //public void SetMovementSpeed(float effect, bool isTemporary = false, float duration = 0)
+    //{
+    //    animator.SetFloat(AnimationKey.MovementSpeed, effect);
+
+    //    if (isTemporary && gameObject.activeInHierarchy)
+    //    {
+    //        if (temporaryMovementSpeed != null)
+    //            StopCoroutine(temporaryMovementSpeed);
+
+    //        temporaryMovementSpeed = StartCoroutine(Duration());
+
+    //        IEnumerator Duration()
+    //        {
+    //            yield return new WaitForSeconds(duration);
+    //            animator.SetFloat(AnimationKey.MovementSpeed, 1);
+    //        }
+    //    }
+    //}
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    //private void OnDisable()
+    //{
+    //    StopAllCoroutines();
+
+    //    if (effectObject)
+    //        Destroy(effectObject);
+
+    //    animator.SetFloat(AnimationKey.AttackSpeed, attackSpeed);
+    //    animator.SetFloat(AnimationKey.MovementSpeed, 1);
+    //}
+
+    //public virtual void ReviewAnimation()
+    //{
+    //    if (!animator)
+    //        return;
+
+    //    animator.SetTrigger(AnimationKey.Review.ToString());
+    //}
+}
+
+public struct AnimationKey
+{
+    public const string WalkBlend = "WalkBlend";
+    public const string Xaxis = "Xaxis";
+    public const string Yaxis = "Yaxis";
+    //public const string IsRun = "IsRun";
+}
--- Assets/Code/Scripts/PlayerAgentMovement.cs
+++ Assets/Code/Scripts/PlayerAgentMovement.cs
@@ -6,36 +6,43 @@ using UnityEngine.AI;
 public class PlayerAgentMovement : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private AnimationSystemBase animationSystemBase;
     private bool isMoving { get; set; }
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animationSystemBase = GetComponentInChildren<AnimationSystemBase>();
     }
 
     private void Update()
     {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isRun = Input.GetKeyDown(KeyCode.LeftShift);
 
         if (input.magnitude <= 0)
         {
-            //anim setbool wwalk false
-            //return
+            Stop();
+            return;
         }
 
         if (Mathf.Abs(input.y) >= 0.01f || Mathf.Abs(input.x) >= 0.01f)
         {
-            Move(input);
+
+            Move(input, isRun);
         }
     }
 
-    private void Move(Vector2 input)
+    private void Move(Vector2 input, bool isRun)
     {
-        //anim walk true
         Vector3 destination = transform.position + Vector3.right * input.x + Vector3.forward * input.y;
         navMeshAgent.destination = destination;
-        //navMeshAgent.Move(destination);
+
+        animationSystemBase.SetMove(AnimationKey.Xaxis, input.x, AnimationKey.Yaxis, input.y, isRun);
     }
 
-    
+    private void Stop()
+    {
+        animationSystemBase.SetMove(AnimationKey.Xaxis, 0f, AnimationKey.Yaxis, 0f, AnimationKey.WalkBlend, 0f);
+    }
 }
